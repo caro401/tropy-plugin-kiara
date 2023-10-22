8f822da27e82c03b707859831b6aa8e75e78b38b
Caro Appleby <caroline.appleby@cantab.net>
2023-10-22 23:31:05 +0100

    Don't provide import, rename plugin

--- src/plugin.js
+++ src/plugin.js
@@ -4,7 +4,7 @@
 // is loaded into Tropy this has to be a CommonJS module. You can use `require`
 // to access the Node.js and Electron APIs or any files bundled with your plugin.
 
-class ExamplePlugin {
+class KiaraPlugin {
 
   // A Tropy plugin is JavaScript class/constructor function. An instance will be
   // created at start-up in each project window for each set of `options` configured
@@ -13,7 +13,7 @@ class ExamplePlugin {
 
     // It is good practice to define a default configuration to use as a fallback
     // in case some options are left blank.
-    this.options = Object.assign({}, ExamplePlugin.defaults, options)
+    this.options = Object.assign({}, KiaraPlugin.defaults, options)
 
     // The plugin instance receives a `context` object from Tropy. Typically,
     // you will store a reference here, so that you can use it later when a
@@ -36,36 +36,13 @@ class ExamplePlugin {
     // This logs the data supplied to the export hook. The data includes
     // the currently selected Tropy items (or all items, if none are currently
     // selected and you triggered the export via the menu).
-    console.log(data)
-  }
-
-  // This method gets called when the import hook is triggered.
-  async import(payload) {
-    this.logger.trace('Called import hook from example plugin')
-
-    // This logs the payload received by the import hook. After this method
-    // completes, Tropy's import command will continue its work with this
-    // payload.
-    console.log(payload)
-
-    // After this method completes, Tropy's import command will continue its
-    // work with this payload. To have Tropy import JSON-LD data, you can
-    // add it here:
-    payload.data = [
-      // Add your items here!
-    ]
-
-    // Alternatively, to import a list of supported local files or remote
-    // URLs you can add the respective arrays instead:
-    //
-    // payload.files = []
-    // payload.urls = []
+    console.log({data})
   }
 }
 
-ExamplePlugin.defaults = {
+KiaraPlugin.defaults = {
   clipboard: false
 }
 
 // The plugin must be the module's default export.
-module.exports = ExamplePlugin
+module.exports = KiaraPlugin
